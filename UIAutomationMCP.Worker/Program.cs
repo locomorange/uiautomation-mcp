6ec0e66a9bc3add4f8275cd2580fe2d78439ce9a
yk sh <yksh@DESKTOP-U5EHR45>
2025-07-11 00:04:10 +0900

    feat: Add multiple new UIAutomation operations for enhanced functionality

--- UIAutomationMCP.Worker/Program.cs
+++ UIAutomationMCP.Worker/Program.cs
@@ -11,6 +11,14 @@ using UIAutomationMCP.Worker.Operations.ControlType;
 using UIAutomationMCP.Worker.Operations.Grid;
 using UIAutomationMCP.Worker.Operations.ElementInspection;
 using UIAutomationMCP.Worker.Operations.Layout;
+using UIAutomationMCP.Worker.Operations.MultipleView;
+using UIAutomationMCP.Worker.Operations.Range;
+using UIAutomationMCP.Worker.Operations.Screenshot;
+using UIAutomationMCP.Worker.Operations.Selection;
+using UIAutomationMCP.Worker.Operations.Table;
+using UIAutomationMCP.Worker.Operations.Text;
+using UIAutomationMCP.Worker.Operations.TreeNavigation;
+using UIAutomationMCP.Worker.Operations.Window;
 using UIAutomationMCP.Worker.Helpers;
 using System.Text.Json;
 
@@ -65,6 +73,56 @@ namespace UIAutomationMCP.Worker
             builder.Services.AddKeyedSingleton<IUIAutomationOperation, ExpandCollapseElementOperation>("ExpandCollapseElement");
             builder.Services.AddKeyedSingleton<IUIAutomationOperation, ScrollElementOperation>("ScrollElement");
             builder.Services.AddKeyedSingleton<IUIAutomationOperation, ScrollElementIntoViewOperation>("ScrollElementIntoView");
+
+            // MultipleView operations
+            builder.Services.AddKeyedSingleton<IUIAutomationOperation, GetAvailableViewsOperation>("GetAvailableViews");
+            builder.Services.AddKeyedSingleton<IUIAutomationOperation, GetCurrentViewOperation>("GetCurrentView");
+            builder.Services.AddKeyedSingleton<IUIAutomationOperation, GetViewNameOperation>("GetViewName");
+            builder.Services.AddKeyedSingleton<IUIAutomationOperation, SetViewOperation>("SetView");
+
+            // Range operations
+            builder.Services.AddKeyedSingleton<IUIAutomationOperation, GetRangePropertiesOperation>("GetRangeProperties");
+            builder.Services.AddKeyedSingleton<IUIAutomationOperation, GetRangeValueOperation>("GetRangeValue");
+            builder.Services.AddKeyedSingleton<IUIAutomationOperation, SetRangeValueOperation>("SetRangeValue");
+
+            // Screenshot operations
+            builder.Services.AddKeyedSingleton<IUIAutomationOperation, TakeScreenshotOperation>("TakeScreenshot");
+
+            // Selection operations
+            builder.Services.AddKeyedSingleton<IUIAutomationOperation, AddToSelectionOperation>("AddToSelection");
+            builder.Services.AddKeyedSingleton<IUIAutomationOperation, ClearSelectionOperation>("ClearSelection");
+            builder.Services.AddKeyedSingleton<IUIAutomationOperation, GetSelectionOperation>("GetSelection");
+            builder.Services.AddKeyedSingleton<IUIAutomationOperation, RemoveFromSelectionOperation>("RemoveFromSelection");
+            builder.Services.AddKeyedSingleton<IUIAutomationOperation, SelectElementOperation>("SelectElement");
+            builder.Services.AddKeyedSingleton<IUIAutomationOperation, SelectItemOperation>("SelectItem");
+
+            // Table operations
+            builder.Services.AddKeyedSingleton<IUIAutomationOperation, GetColumnHeadersOperation>("GetColumnHeaders");
+            builder.Services.AddKeyedSingleton<IUIAutomationOperation, GetRowHeadersOperation>("GetRowHeaders");
+            builder.Services.AddKeyedSingleton<IUIAutomationOperation, GetTableInfoOperation>("GetTableInfo");
+
+            // Text operations
+            builder.Services.AddKeyedSingleton<IUIAutomationOperation, AppendTextOperation>("AppendText");
+            builder.Services.AddKeyedSingleton<IUIAutomationOperation, FindTextOperation>("FindText");
+            builder.Services.AddKeyedSingleton<IUIAutomationOperation, GetSelectedTextOperation>("GetSelectedText");
+            builder.Services.AddKeyedSingleton<IUIAutomationOperation, GetTextAttributesOperation>("GetTextAttributes");
+            builder.Services.AddKeyedSingleton<IUIAutomationOperation, GetTextOperation>("GetText");
+            builder.Services.AddKeyedSingleton<IUIAutomationOperation, GetTextSelectionOperation>("GetTextSelection");
+            builder.Services.AddKeyedSingleton<IUIAutomationOperation, SelectTextOperation>("SelectText");
+            builder.Services.AddKeyedSingleton<IUIAutomationOperation, SetTextOperation>("SetText");
+            builder.Services.AddKeyedSingleton<IUIAutomationOperation, TraverseTextOperation>("TraverseText");
+
+            // TreeNavigation operations
+            builder.Services.AddKeyedSingleton<IUIAutomationOperation, GetAncestorsOperation>("GetAncestors");
+            builder.Services.AddKeyedSingleton<IUIAutomationOperation, GetChildrenOperation>("GetChildren");
+            builder.Services.AddKeyedSingleton<IUIAutomationOperation, GetDescendantsOperation>("GetDescendants");
+            builder.Services.AddKeyedSingleton<IUIAutomationOperation, GetElementTreeOperation>("GetElementTree");
+            builder.Services.AddKeyedSingleton<IUIAutomationOperation, GetParentOperation>("GetParent");
+            builder.Services.AddKeyedSingleton<IUIAutomationOperation, GetSiblingsOperation>("GetSiblings");
+
+            // Window operations
+            builder.Services.AddKeyedSingleton<IUIAutomationOperation, TransformElementOperation>("TransformElement");
+            builder.Services.AddKeyedSingleton<IUIAutomationOperation, WindowActionOperation>("WindowAction");
 
             // Register Worker service
             builder.Services.AddSingleton<WorkerService>();
